e01568e8b5826f582ed43ab98bef9b62d2c42486
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-16 20:58:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,6 +57,23 @@ module "ec2_instance" {
 
 }
 
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = var.securitygroup_name
+  description = "Security group for EC2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_ipv6_cidr_blocks = ["::/0"]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp", "ssh-tcp"]
+  egress_rules        = ["all-all"]
+
+  tags = var.securitygroup_tags
+}
+
 
 resource "aws_kms_key" "this" {
 }
