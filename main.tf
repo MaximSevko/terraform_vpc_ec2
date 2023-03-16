c24ffd93c0df6959ad10115a7592f8bfb184cb7f
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-16 22:08:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "ec2_instance" {
   // Input variables
   name                         = var.ec2_name
   ami                          = "ami-05a66dc4a507a82cc"
-  instance_type                = "t4g.micro"
+  instance_type                = "t4g.small"
   availability_zone            = element(module.vpc.azs, 0)
   subnet_id                    = element(module.vpc.public_subnets, 0)
   vpc_security_group_ids       = [module.security_group.this_security_group_id]
