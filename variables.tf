e2fe04e9322fe42c4a47a3cba19b195ff411ae2b
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-16 21:57:15 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,56 @@
 variable "aws_region" {
-    default = "us-east-1"
+  description = "The AWS region to deploy to."
+  default     = "us-east-1"
 }
 
 variable "env" {
-    description = "enviroment"
-    default = "dev"
+  description = "The environment for the deployment."
+  default     = "dev"
 }
 
 variable "AWS_Region" {
-    default = "eu-central-1"
+  description = "The AWS region to deploy resources to."
+  default     = "us-east-1"
 }
 
 variable "vpc_name" {
-  description = "Name of VPC"
+  description = "The name of the VPC to be created."
   type        = string
   default     = "vpc3"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "The CIDR block for the VPC."
   type        = string
   default     = "10.81.0.0/16"
 }
 
 variable "vpc_intra_subnets" {
-  description = "Intra subnets for VPC"
+  description = "The list of CIDR blocks for the intra subnets of the VPC."
   type        = list(string)
   default     = ["10.81.1.0/24", "10.81.2.0/24", "10.81.3.0/24"]
 }
 
 variable "vpc_public_subnets" {
-  description = "Public subnets for VPC"
+  description = "The list of CIDR blocks for the public subnets of the VPC."
   type        = list(string)
   default     = ["10.81.101.0/24", "10.81.102.0/24", "10.81.103.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
-  description = "Enable NAT gateway for VPC"
+  description = "Set to true to enable a NAT gateway for the VPC."
   type        = bool
   default     = false
 }
 
 variable "securitygroup_name" {
+  description = "The name of the security group to be created."
   type        = string
   default     = "amilinux_security_group"
 }
 
 variable "ec2_name" {
+  description = "The name of the EC2 instance to be created."
   type        = string
   default     = "Amazon Linux 2 Instance"
 }
-  
