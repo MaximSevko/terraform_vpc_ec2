6b96ca4c64d888930f38c6a5609ec20061b8807b
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-16 21:35:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,24 @@ module "vpc" {
 
 }
 
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+
+  name        = var.securitygroup_name
+  description = "Security group for EC2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_ipv6_cidr_blocks = ["::/0"]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp", "ssh-tcp"]
+  egress_rules        = ["all-all"]
+
+}
+
+
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
@@ -27,7 +45,7 @@ module "ec2_instance" {
   instance_type                = "t4g.micro"
   availability_zone            = element(module.vpc.azs, 0)
   subnet_id                    = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids       = [module.security_group_id]
+  vpc_security_group_ids       = module.security_group.security_group_id
   associate_public_ip_address  = true
   key_name                     = "keym"
   monitoring                   = true
@@ -55,20 +73,3 @@ module "ec2_instance" {
   ]
 
 }
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 3.0"
-
-  name        = var.securitygroup_name
-  description = "Security group for EC2 instance"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_ipv6_cidr_blocks = ["::/0"]
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp", "ssh-tcp"]
-  egress_rules        = ["all-all"]
-
-}
-
