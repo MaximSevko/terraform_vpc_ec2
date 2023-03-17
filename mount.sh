34c49ba0753a0d81ea1a18f572c00d7ae8989c72
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-17 17:31:43 +0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
-mkdir /home/ec2-user/disk2
-mkdir /home/ec2-user/disk3
 
-echo $(blkid | grep /dev/nvme1n1 | awk '{print $3}') /home/ec2-user/disk2 xfs defaults 1 1 >> /etc/fstab
-echo $(blkid | grep /dev/nvme2n1 | awk '{print $3}') /home/ec2-user/disk3 xfs defaults 1 1 >> /etc/fstab
+sudo yum install xfsprogs -y
+
+sudo mkdir /home/ec2-user/disk2
+sudo mkdir /home/ec2-user/disk3
+
+
+sudo mkfs -t xfs /dev/nvme1n1
+sudo mkfs -t xfs /dev/nvme2n1
+
+echo $(blkid | grep /dev/nvme1n1 | awk '{print $2}') /home/ec2-user/disk2 xfs defaults 1 1 >> /etc/fstab
+echo $(blkid | grep /dev/nvme2n1 | awk '{print $2}') /home/ec2-user/disk3 xfs defaults 1 1 >> /etc/fstab
+
+sudo mount -a
