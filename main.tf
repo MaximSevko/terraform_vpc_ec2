b259c5ae2c9a6e3c6f9b48ba2db47fa759172689
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-22 01:48:29 +0300

    Route 53 added
    Route 53 added

--- main.tf
+++ main.tf
@@ -122,3 +122,16 @@ resource "aws_ebs_volume" "disk2" {
 
 resource "aws_kms_key" "this" {
 }
+
+resource "aws_route53_zone" "dev" {
+  name = var.route53_zone_name
+  tags = var.route53_tags
+}
+
+resource "aws_route53_record" "record1" {
+  zone_id = aws_route53_zone.dev.zone_id
+  name    = var.route53_record_name
+  type    = "A"
+  ttl     = 300
+  records = [module.ec2_instance.public_ip]
+}
